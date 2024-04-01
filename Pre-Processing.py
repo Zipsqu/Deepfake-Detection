f80f4f2f57c01197b9fc1e7bd25e5f4a94a6567a
Zipsqu <163416599+Zipsqu@users.noreply.github.com>
2024-04-01 13:55:57 +0100

    Update Pre-Processing.py

--- Pre-Processing.py
+++ Pre-Processing.py
@@ -1,15 +1,8 @@
-#This code does the pre-processing as discused in README. 
-#Replace xxx in the path as required.
-#Each of this files usually contains destination folder (for dataset), and the destination (or multiple ones in case of dataset split), these should be adjusted as required. 
+# Both of these codes are contating variables that should be adjusted (such as destination/output directory).
+# Path to these codes should be adjusted as required (specify XXX).
 
-#Face & Landmark extraction, this code doesn't use GPU so it can take a fair amount of time (it's efficient on the CPU- this allows tasks in the background).
+#Face & Landmark extraction, this code uses multi-processing so it is quiet heavy on CPU.
 python "XXX/Pre-Processing/Face & Landmark Extraction.py"
-
-#Re-sizing the images to 224x224 & moving to new folder.
-python "XXX/Pre-Processing/Re-sizing.py"
-
-#Adjust the JSON metadata files to resizing & moving to same folder as images.
-python "XXX/Pre-Processing/Adjusting JSON.py"
 
 #Splitting dataset into training/validation/testing (80%,10%,10%), while making sure frames related to one video will stay in same folder.
 python "XXX/Pre-Processing/Dataset Split.py"
