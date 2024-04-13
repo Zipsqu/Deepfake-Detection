ce794e9d48233f6c3436803ece11ef73c5928008
Zipsqu <163416599+Zipsqu@users.noreply.github.com>
2024-04-13 13:23:37 +0100

    Update train.py

--- GANSs/train.py
+++ GANSs/train.py
@@ -10,7 +10,9 @@ input_shape = (64, 64, 3)
 latent_dim = 100
 epochs = 100
 batch_size = 64
-model_save_path = 'path/to/save/discriminator/model.h5'  # Specify the path to save the model
+model_save_path = 'D:/Dataset/model.h5'  # Specify the path to save the model
+train_dirs = ['D:/Dataset/Training']
+val_dir = 'D:/Dataset/Validation'
 
 # Build and compile the discriminator
 discriminator = build_discriminator(input_shape)
@@ -27,9 +29,6 @@ combined = tf.keras.Model(generator.input, validity)
 combined.compile(loss='binary_crossentropy', optimizer='adam')
 
 # Preprocess data
-train_dirs = ['path/to/real/training/dir', 'path/to/fake/training/dir']
-val_dir = 'path/to/validation/dir'
-
 (x_train, y_train), (x_val, y_val) = preprocess_data(train_dirs, val_dir)
 
 # Training loop
@@ -41,22 +40,20 @@ for epoch in range(epochs):
     
     # Mini-batch training
     for i in range(0, len(x_train), batch_size):
-        real_images = x_train_shuffled[i:i+batch_size]
-        real_labels = y_train_shuffled[i:i+batch_size]
+        batch_images = x_train_shuffled[i:i+batch_size]
+        batch_labels = y_train_shuffled[i:i+batch_size]
         
-        # Train discriminator on real images
-        d_loss_real = discriminator.train_on_batch(real_images, real_labels)
-        
-        # Train discriminator on fake images
-        noise = np.random.normal(0, 1, (batch_size, latent_dim))
-        fake_images = generator.predict(noise)
-        fake_labels = np.zeros(batch_size)
-        d_loss_fake = discriminator.train_on_batch(fake_images, fake_labels)
+        # Train discriminator
+        d_loss = discriminator.train_on_batch(batch_images, batch_labels)
         
         # Train generator (combined model)
         noise = np.random.normal(0, 1, (batch_size, latent_dim))
         valid_labels = np.ones(batch_size)
         g_loss = combined.train_on_batch(noise, valid_labels)
+    
+    # Evaluate on validation data
+    val_loss, val_accuracy = discriminator.evaluate(x_val, y_val, verbose=0)
+    print(f"Epoch {epoch+1}/{epochs}, Discriminator Validation Loss: {val_loss}, Discriminator Validation Accuracy: {val_accuracy}")
 
 # Save the trained discriminator model
 discriminator.save(model_save_path)
